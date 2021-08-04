bbcf06f332ad12eddfa57db44fa90dc477084f30
Ufuoma Ekemike <uekemike@yahoo.com>
2021-08-04 02:42:34 -0400

    Update html and js

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+var key ="";
+var city = document.querySelector(".state");
+var url=""
+
+
+/* listen for a click on city or click on search button to get the
+
+use queryselector all
+use addeventlistener
+*/
+
+function clickState(){
+    if ('condition is true'){
+    "do something"
+}
+}
+
+function clickSearch(){
+    if ('condition is true'){
+        "do something"
+    }
+}
