bc1f408c18cb9203f8abe91a155761cc096dbe9b
Ufuoma Ekemike <uekemike@yahoo.com>
2021-08-05 02:25:27 -0400

    updated js with fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 var submitBtn =document.querySelector('.submitBtn')
 var city = document.querySelector('.searchByCityName');
+var cityName = document.querySelector('.city');
+var country = document.querySelector('.country');
 var dateTag = document.querySelector('.dateTag');
-var wind = document.querySelector('.wind');
+var weather = document.querySelector('.weather');
 var humidity = document.querySelector('.humidity');
-var uvIndex = document.querySelector('.uvIndex');
+var feelsLike =document.querySelector('feelsLike');
+
 /*('https://api.openweathermap.org/data/2.5/weather?q='+city.value+'&appid=7dfee20f8b9610de4e24031ae9190e5d')*/
 
 
@@ -16,12 +19,27 @@ use addeventlistener
 function getForecast(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+city.value+'&appid=7dfee20f8b9610de4e24031ae9190e5d')
         .then(response => response.json())
-        .then(data =>console.log(data))
-        /*.then(data=>{
-            var wind =data['wind']
+        .then(data=>{
+            var cityNameValue =data['name'];
+            var countryNameValue =data['sys']['country'];
+            var weatherValue =data['weather'][0]['description'];
+            var humidityValue =data['main']['humidity'];
+            var feelsLike =data['main']['feels_like'];
+        
 
+            //var feelslike =date.main.feels_like;
+            
+            
+            cityName.innerHTML="City: " + cityNameValue;
+            country.innerHTML="Country: " + countryNameValue;
+            weather.innerHTML="Weather: " + weatherValue;
+            humidity.innerHTML="Humidity: " + humidityValue;
+            feelsLike.innerHTML="Humidity: " + feelsLike;
+
+            
         })
-        */
+        .catch(err=> alert("Wrong City Name!"))
+       
 }
 
 submitBtn.addEventListener("click", getForecast)
