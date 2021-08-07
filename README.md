c351a433915e98898d6e5ae565568590778b628e
Ufuoma Ekemike <uekemike@yahoo.com>
2021-08-07 13:04:31 -0400

    update

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Weather-Dashboard
 
-This weather dashboard is a 5 day Weather forecast. Information on this page is pulled by using a weather API.
+This weather dashboard is a 5 day Weather forecast. Search is by City. Information on this page is pulled by using a weather API.
+
+
+![image](https://user-images.githubusercontent.com/84823764/128608161-52fedec7-3d7b-4864-b64a-89109ca3ba45.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,49 +2,45 @@ var submitBtn =document.querySelector('.submitBtn')
 var city = document.querySelector('.searchByCityName');
 var cityName = document.querySelector('.city');
 var country = document.querySelector('.country');
-var dateTag = document.querySelector('.dateTag');
+//var dateTag = document.querySelector('.dateTag');
 var weather = document.querySelector('.weather');
 var humidity = document.querySelector('.humidity');
-var feelsLike =document.querySelector('feelsLike');
-
-/*('https://api.openweathermap.org/data/2.5/weather?q='+city.value+'&appid=7dfee20f8b9610de4e24031ae9190e5d')*/
+var weatherIcon = document.querySelector('.weatherIcon');
 
 
-/* listen for a click on search button
-
-use queryselector all
-use addeventlistener
-*/
 
 function getForecast(){
+ 
+
+
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+city.value+'&appid=7dfee20f8b9610de4e24031ae9190e5d')
         .then(response => response.json())
+        //.then(data => console.log(data))
         .then(data=>{
             var cityNameValue =data['name'];
             var countryNameValue =data['sys']['country'];
             var weatherValue =data['weather'][0]['description'];
-            var humidityValue =data['main']['humidity'];
+            var humidityValue =data['main']['temp'];
+            var weatherIcon = data['weather'][0]['icon'];
        
-        
-
-            //var feelslike =date.main.feels_like;
-            
-            
             cityName.innerHTML="City: " + cityNameValue;
             country.innerHTML="Country: " + countryNameValue;
             weather.innerHTML="Weather: " + weatherValue;
             humidity.innerHTML="Humidity: " + humidityValue;
-           
-
+            weatherIcon.innerHTML= '<img src ="http://openweathermap.org/img/w' + weatherIcon + ".png" + '"/>';
             
+                    
         })
-        .catch(err=> alert("Wrong City Name!"))
-       
+               
 }
 
 submitBtn.addEventListener("click", getForecast)
 
 
+function getCurrentDay(){
+var currentDay = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+$("#currentDay").text(currentDay);}
+/**/
 
-/*var currentDay = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
- $("#currentDay").text(currentDay);*/
+//'https://api.openweathermap.org/data/2.5/forecast?q='+city.value+'&appid=7dfee20f8b9610de4e24031ae9190e5d'
+//'https://api.openweathermap.org/data/2.5/weather?q='+city.value+'&appid=7dfee20f8b9610de4e24031ae9190e5d'
